319e0e766a1ff289d9a0710cf976ee2a4c1ad1da
Jaztin <77910637+csun-jtabu@users.noreply.github.com>
2021-08-10 00:53:12 -0700

    Chapter 9 Circle2 and Circle2Tester
    In this chapter, I went over objects and classes. Specifically, I went over when to use Static and Instance methods/variables and some of the rules regarding them.

---
+++ Circle2.java
@@ -0,0 +1,30 @@
+
+public class Circle2 {
+
+	double radius;
+	
+	static int numberOfObjects = 0;
+	
+	Circle2()
+	{
+		radius = 1.0;
+		numberOfObjects++;
+	}
+	
+	Circle2(double newRadius)
+	{
+		this.radius = newRadius;
+		numberOfObjects++;
+	}
+	
+	static int getNumberOfObjects()
+	{
+		return numberOfObjects;
+	}
+	
+	double getArea()
+	{
+		return radius*radius*Math.PI;
+	}
+	
+}
---
+++ Circle2Tester.java
@@ -0,0 +1,12 @@
+
+
+
+public class Circle2Tester {
+	
+	public static void main(String[]args)
+	{
+		//Circle2 c1 = new Circle2();
+		System.out.println(Circle2.getNumberOfObjects());
+	}
+	
+}
